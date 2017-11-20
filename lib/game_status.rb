9e5e77d0bfebe4468919c724303c081a89bf3e97
Man Leung <lepusleung@gmail.com>
2017-11-20 21:45:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,6 +5,9 @@ end
 
 # Define your WIN_COMBINATIONS constant
 def won?(board)
+  WIN_COMBINATIONS.find do |win_combo|
+    board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
+  end
 end
 
 WIN_COMBINATIONS = [
